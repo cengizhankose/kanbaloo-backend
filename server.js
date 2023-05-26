e347f74d52b22be7be7f8eb27a9f8dc6178d32b7
Cengizhan KOSE <kose651@gmail.com>
2023-05-26 17:58:57 +0300

    update auth ep

--- server.js
+++ server.js
@@ -23,10 +23,11 @@ const requireAuth = (req, res, next) => {
 
 app.post("/api/login", async (req, res) => {
   const { email, password } = req.body;
-  const { user, error } = await supabase.auth.signInWithPassword({
+  const { data: loginData, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
+  console.log("🚀 ~ file: server.js:30 ~ app.post ~ user:", loginData);
   if (error) {
     res.status(400).json({ error: error.message });
     return;
@@ -54,12 +55,23 @@ app.post("/api/login", async (req, res) => {
         throw createError;
       }
 
-      res
-        .status(200)
-        .json({ user, newUser, email, password, message: "logged in" });
+      res.status(200).json({
+        user: loginData.user,
+        session: loginData.session,
+        newUser,
+        email,
+        password,
+        message: "logged in",
+      });
     } else if (existingUsers.length === 1) {
       // Existing user found, proceed with login
-      res.status(200).json({ user, email, password, message: "logged in" });
+      res.status(200).json({
+        user: loginData.user,
+        session: loginData.session,
+        email,
+        password,
+        message: "logged in",
+      });
     } else {
       throw new Error("Multiple users found with the same email.");
     }
@@ -124,6 +136,32 @@ app.put("/api/tasks/:taskId", requireAuth, async (req, res) => {
 app.delete("/api/tasks/:taskId", requireAuth, async (req, res) => {
   // Handle task deletion using req.params data
   // Use Supabase's `supabase.from('tasks').delete()` method
+});
+
+app.get("/boards/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    let { data: boards, error } = await supabase
+      .from("user_boards")
+      .select("board_id")
+      .eq("user_id", userId);
+
+    if (error) throw error;
+
+    const boardIds = boards.map((board) => board.board_id);
+
+    let { data: userBoards, error: boardError } = await supabase
+      .from("boards")
+      .select("*")
+      .in("id", boardIds);
+
+    if (boardError) throw boardError;
+
+    res.json(userBoards);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
